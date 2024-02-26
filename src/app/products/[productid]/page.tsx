ecd9f2c929f89ef08bb351553436fa0cf37148d8
WaiLun99 <wailun.fung@axstarzy.com>
2024-02-27 00:22:14 +0800

    Forth commit

--- src/app/products/[productid]/page.tsx
+++ src/app/products/[productid]/page.tsx
@@ -1,7 +1,26 @@
+import { Metadata } from "next";
+import { resolve } from "path";
+
+type Props ={
+  params:{
+    productid :string
+  }
+}
+
+export const generateMetadata = async ({params} : Props):Promise<Metadata>=>{
+  const title = await new Promise(resolve=>{
+    setTimeout(()=>{
+      resolve(`iPhone ${params.productid}`)
+    },100)
+  })
+  
+  return {
+    title: `Product ${title}`
+  }
+}
+
 export default function ProductDetails({
   params,
-}: {
-  params: { productid: string };
-}) {
+}: Props) {
   return <h1>Details of product {params.productid}</h1>;
 }
